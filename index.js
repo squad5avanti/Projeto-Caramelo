a0425bb93c77f0fa798347514cd0ff12f6c7dde6
Fern <fmenezes098@gmail.com>
2025-09-27 15:13:33 -0300

    :sparkles:  Endpoints da entidade pets

--- index.js
+++ index.js
@@ -11,11 +11,49 @@ app.get("/pets", async (request, response) => {
     return response.json(pets).send();
 });
 app.post("/pets", async (request, response) => {
-    const { nome, especie, idade, descricao, status} = request.body;
-    pets.push({ nome, especie, idade, descricao, status})
-    return response.status(201).json(pets);
-});
+    const { nome, especie, idade, descricao, status } = request.body;
+    const pets = await prismaClient.pets.create({
+        data: { nome, especie, idade, descricao, status }
+    })
 
-app.listen(8080, () => {
-    console.log("Running port 8080")
+    return response.status(201).json(pets);
+})
+
+app.put("/pets/:id", async (request, response) => {
+    const { id } = request.params;
+    const { nome, especie, idade, descricao, status } = request.body;
+
+    const petExist = await prismaClient.pets.findUnique({ 
+        where: { id: parseInt(id) }
+    })
+
+    if (!petExist) {
+        return response.status(404).json({message: "Pet não encontrado"});
+    }
+
+    const pet = await prismaClient.pets.update({
+        data : { nome, especie, idade, descricao, status },
+        where: { id: parseInt(id) }
+    })
+
+    return response.status(200).json(pet);
+})
+
+app.delete("/pets/:id", async (request, response) => {
+    const { id } = request.params;
+    const petExist = await prismaClient.pets.findUnique({ 
+        where: { id: parseInt(id) }
+    })
+
+    if (!petExist) {
+        return response.status(404).json({message: "Pet não encontrado"});
+    }
+
+    return response.status(200).json("Cadastro deletado com sucesso!");
+})
+
+
+
+app.listen(8081, () => {
+    console.log("Running port 8081")
 });
