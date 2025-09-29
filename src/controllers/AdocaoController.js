524d78eb640f572890eb1a764f134ef8b92a4643
Ricardo Souza <ricardo.almeida@maislaudo.com.br>
2025-09-29 19:17:58 -0300

    Ajusta classe AdocaoController para trabalhar na nova organização do projeto

--- src/controllers/AdocaoController.js
+++ src/controllers/AdocaoController.js
@@ -1,56 +1,69 @@
 import { prismaClient } from "../database/PrismaClient.js";
 
-//adoção
-app.get("/adocoes", async (request, response) => {
-    const adocoes = await prismaClient.adocoes.findMany();
-    return response.json(adocoes).send();
-});
+export class AdocaoController {
 
-app.post("/adocoes", async (request, response) => {
-    const { pets_id, adotantes_id, data_adocao  } = request.body;
-    const adocoes = await prismaClient.adocoes.create({
-        data: { pets_id, adotantes_id, data_adocao: new Date (data_adocao) }
-    })
+    async obterTodasAdocoes (request, response) {
+        try {
+            const adocoes = await prismaClient.adocoes.findMany();
+            return response.json(adocoes).send();
+        } catch (error) {
+            return response.status(500).json(error);
+        }
+    }
+    
+    async salvarAdocao (request, response) {
+        try{
+            const { pets_id, adotantes_id, data_adocao  } = request.body;
+            const adocoes = await prismaClient.adocoes.create({
+                data: { pets_id, adotantes_id, data_adocao: new Date (data_adocao) }
+            });
+            return response.status(201).json(adocoes);
+        } catch (error) {
+            return response.status(500).json(error);
+        }
+    }
+    
+    async atualizarAdocao (request, response) {
+        try {
+            const { id } = request.params;
+            const { pets_id, adotantes_id, data_adocao } = request.body;
+        
+            const adoptionExist = await prismaClient.adocoes.findUnique({ 
+                where: { adocao_id: parseInt(id) }
+            });
 
-    return response.status(201).json(adocoes);
-})
+            if (!adoptionExist) {
+                return response.status(404).json({message: "Adoção não encontrada"});
+            }
 
-app.put("/adocoes/:id", async (request, response) => {
-    const { id } = request.params;
-    const { pets_id, adotantes_id, data_adocao } = request.body;
-
-    const adoptionExist = await prismaClient.adocoes.findUnique({ 
-        where: { adocao_id: parseInt(id) }
-    })
-
-    if (!adoptionExist) {
-        return response.status(404).json({message: "Adoção não encontrada"});
+            const adocao = await prismaClient.adocoes.update({
+                data : { pets_id, adotantes_id, data_adocao: new Date (data_adocao) },
+                where: { adocao_id: parseInt(id) }
+            });
+    
+            return response.status(200).json(adocao);
+        } catch (error) {
+            return response.status(500).json(error);
+        }
     }
 
-    const adocao = await prismaClient.adocoes.update({
-        data : { pets_id, adotantes_id, data_adocao: new Date (data_adocao) },
-        where: { adocao_id: parseInt(id) }
-    })
-
-    return response.status(200).json(adocao);
-})
-
-app.delete("/adocoes/:id", async (request, response) => {
-    const { id } = request.params;
-    const adoptionExist = await prismaClient.adocoes.findUnique({ 
-        where: { adocao_id: parseInt(id) }
-    })
-
-    if (!adoptionExist) {
-        return response.status(404).json({message: "Adoção não encontrada"});
+    async removerAdocao (request, response) {
+        try {
+            const { id } = request.params;
+            const adoptionExist = await prismaClient.adocoes.findUnique({ 
+                where: { adocao_id: parseInt(id) }
+            });
+    
+            if (!adoptionExist) {
+                return response.status(404).json({message: "Adoção não encontrada"});
+            }
+            await prismaClient.adocoes.delete({
+                where: { adocao_id: parseInt(id) }
+            });
+    
+            return response.status(200).json("Cadastro deletado com sucesso!");
+        } catch (error) {
+            return response.status(500).json(error);
+        }
     }
-    await prismaClient.adocoes.delete({
-    where: { adocao_id: parseInt(id) }
-  });
-
-    return response.status(200).json("Cadastro deletado com sucesso!");
-})
-
-app.listen(8081, () => {
-    console.log("Running port 8081")
-});
+}
