1130703d9ccbd255e564847976cd859528b6e135
Ricardo Souza <ricardo.almeida@maislaudo.com.br>
2025-09-29 19:18:38 -0300

    Adiciona rotas para os endpoints da classe AdocaoController

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,20 @@
 import { Router } from "express"
 import { AdotanteController } from "../controllers/AdotanteController.js";
+import { AdocaoController } from "../controllers/AdocaoController.js";
 
 const router = Router();
 const adotanteController = new AdotanteController();
+const adocaoController = new AdocaoController();
 
 
-router.get("/adotantes", adotanteController.encontrarTodosAdotantes);
+router.get("/adotantes", adotanteController.obterTodosAdotantes);
 router.post("/adotantes", adotanteController.salvarAdotante);
 router.put("/adotantes/:id", adotanteController.atualizarAdotante);
 router.delete("/adotantes/:id", adotanteController.removerAdotante);
+
+router.get("/adocoes", adocaoController.obterTodasAdocoes);
+router.post("/adocoes", adocaoController.salvarAdocao);
+router.put("/adocoes/:id", adocaoController.atualizarAdocao);
+router.delete("/adocoes/:id", adocaoController.removerAdocao);
 
 export default router
