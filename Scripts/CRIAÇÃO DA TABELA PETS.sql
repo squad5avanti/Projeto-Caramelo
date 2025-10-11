eaebf6ac83e586f421657c1b8e7faace20b14ddd
Gabriel Estelet <65242022+Gabriel-Estelet@users.noreply.github.com>
2025-10-11 12:44:24 -0300

    Update CRIAÇÃO DA TABELA PETS.sql
    Alterações da tabela Pets para refletir situação atual do projeto: inclusão de colunas "vacinado", "peso", "sexo" e "situacao"; adição do status "reservado" como opção da coluna "status".

--- Scripts/CRIAÇÃO DA TABELA PETS.sql
+++ Scripts/CRIAÇÃO DA TABELA PETS.sql
@@ -1,8 +1,14 @@
 -- CRIAÇÃO DE TIPO PERSONALIZADO PARA CAMPO "status" NA TABELA Pets
-CREATE TYPE ESTADO AS ENUM ('adotado', 'disponivel');
+CREATE TYPE ESTADO AS ENUM ('reservado', 'adotado', 'disponivel');
 
 -- CRIAÇÃO DE TIPO PERSONALIZADO PARA CAMPO "tamanho" NA TABELA Pets
 CREATE TYPE TAMANHO AS ENUM ('pequeno', 'medio', 'grande');
+
+-- CRIAÇÃO DE TIPO PERSONALIZADO PARA CAMPO "situacao" NA TABELA Pets
+CREATE TYPE SITUACAO AS ENUM ('pendente', 'aceito');
+
+-- CRIAÇÃO DE TIPO PERSONALIZADO PARA CAMPO "sexo" NA TABELA Pets
+CREATE TYPE SEXO AS ENUM ('macho', 'femea');
 
 -- CRIAÇÃO DA TABELA Pets CONFORME REQUISITOS
 CREATE TABLE Pets
@@ -13,6 +19,10 @@ CREATE TABLE Pets
     nascimento DATE NOT NULL,
     descricao VARCHAR(100),
     estado ESTADO NOT NULL,
+    situacao SITUACAO NOT NULL,
+    vacinado BOOLEAN NOT NULL,
+    peso INT, 
+    sexo SEXO NOT NULL,
     tamanho TAMANHO,
     personalidade VARCHAR(50),
     avatar TEXT,
