7ba4c48bd6a480b008ccfa4183ae3a5d9f5ef26b
Ricardo Souza <ricardoalmeidasouza96@gmail.com>
2025-10-21 21:46:54 -0300

    Configura permissão para o front-end acessar a API

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 import router from "./routes/index.js";
 
 const app = express();
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+}));
+
 app.use(express.json());
 app.use(router);
 
