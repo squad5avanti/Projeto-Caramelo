cf90925d8badbde29b426a7f09bc21dc615a397d
Gabriel Estelet <65242022+Gabriel-Estelet@users.noreply.github.com>
2025-10-23 17:48:59 -0300

    Update CRIAÇÃO DA TABELA USUARIOS.sql

--- Scripts/CRIAÇÃO DA TABELA USUARIOS.sql
+++ Scripts/CRIAÇÃO DA TABELA USUARIOS.sql
@@ -6,7 +6,7 @@ CREATE TABLE Usuarios
     telefone VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     senha TEXT NOT NULL,
-    usuarioAdmin BOOLEAN DEFAULT FALSE
+    usuarioAdmin BOOLEAN DEFAULT FALSE,
     pets_favoritos INT NOT NULL,
     CONSTRAINT pets_favoritos_fk
     FOREIGN KEY (pets_favoritos) REFERENCES Pets(id)
